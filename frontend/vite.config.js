57e285dc869358e9c0a9ac3114fac19a51c007cb
mahesh <srbalaji2024@gmail.com>
2025-11-28 14:34:16 +0530

    frontend changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  server: {
+    open: true,
+    port: 3000,
+  },
+  plugins: [react(), tailwindcss()],
+});
